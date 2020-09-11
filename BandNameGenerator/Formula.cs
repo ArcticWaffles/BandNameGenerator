fd1c60cf7c8593d0fc1a32408fcb9e82811148c6
Tisa Foster <tisa.foster@gmail.com>
2020-09-10 20:26:02 -0600

    Trying out using a ViewModel. Added formula class.

---
+++ BandNameGenerator/Formula.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BandNameGenerator
+{
+    public class Formula
+    {
+        public enum Part
+        {
+            SingularNoun,
+            PluralNoun,
+            Adjective,
+            SingularNounPossessive,
+            Number,
+            Verb,
+            InitialThe,
+            OfThe
+        }
+
+        private readonly List<Part> parts;
+
+        private readonly static List<string> numbers = new List<string>() { "3", "12", "100", "500", "A Thousand" };
+
+        public Formula(List<Part> parts)
+        {
+            this.parts = parts;
+        }
+
+        public string GetValue(IGenre genre)
+        {
+            var strings =
+                from part in parts
+                select part switch
+                {
+                    Part.SingularNoun => GetStringFromCollection(genre.SingularNouns),
+                    Part.PluralNoun => GetStringFromCollection(genre.PluralNouns),
+                    Part.Adjective => GetStringFromCollection(genre.Adjectives),
+                    Part.SingularNounPossessive => GetStringFromCollection(genre.SingularNouns) + "'s",
+                    Part.Number => GetStringFromCollection(numbers),
+                    Part.Verb => GetStringFromCollection(genre.Verbs),
+                    Part.InitialThe => "The",
+                    Part.OfThe => "of the",
+                    _ => "",
+                };
+            return string.Join(' ', strings);
+        }
+        //TODO: Unit tests?
+
+        private string GetStringFromCollection(List<string> collection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BandNameGenerator/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using BandNameGenerator.Genres;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandNameGenerator
+{
+    public class ViewModel
+    {
+        public enum Genre
+        {
+            Metal,
+            Country,
+            Folk
+        }
+
+        public ViewModel()
+        {
+        }
+
+        private Genre selectedGenre = Genre.Metal;
+        public Genre SelectedGenre
+        {
+            get
+            {
+                return selectedGenre;
+            }
+            set
+            {
+                selectedGenre = value;
+                BandName = "";
+            }
+        }
+
+        public string BandName { get; set; }
+
+        public void GenerateName()
+        {
+            BandName = "Best band name evah";
+        }
+
+        private IGenre convertToIGenre(Genre genre)
+        {
+            return genre switch
+            {
+                Genre.Metal => new Metal(),
+                Genre.Country => new Country(),
+                Genre.Folk => new Folk(),
+                _ => new Metal(),
+            };
+        }
+    }
+}
